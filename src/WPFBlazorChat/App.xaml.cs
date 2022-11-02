ba31ac4deaef79f69f8d908c962cebef75df614d
Dotnet9 <1012434131@qq.com>
2022-11-02 15:02:28 +0800

    添加一些可有可无的东西；

--- src/WPFBlazorChat/App.xaml.cs
+++ src/WPFBlazorChat/App.xaml.cs
@@ -8,6 +8,7 @@ public partial class App
 {
     public App()
     {
+        // 订阅打开聊天窗口消息，在主窗口点击用户时，确认后会发送此消息
         Messenger.Default.Subscribe<OpenWeChatMessage>(this, msg =>
         {
             var chatWin = new WeChatWindow(msg.User);
