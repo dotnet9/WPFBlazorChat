dbffb12178ab291d48e0d40b3995879033eb189d
Dotnet9 <1012434131@qq.com>
2022-11-02 16:09:25 +0800

    模拟数据做成资源文件形式嵌入共享库；

--- src/WPFBlazorChat.Shared/Services/UserService.cs
+++ src/WPFBlazorChat.Shared/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using WPFBlazorChat.Shared.Models;
 
 namespace WPFBlazorChat.Shared.Services;
@@ -9,7 +10,13 @@ public class UserService : IUserService
 
     public List<User>? GetUsers()
     {
-        return _users ??= JsonSerializer.Deserialize<List<User>>(
-            System.IO.File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}Datas/user.json"));
+        if (_users is { Count: > 0 })
+        {
+            return _users;
+        }
+
+        using var stream = new MemoryStream(Resources.users);
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+        return _users ??= JsonSerializer.Deserialize<List<User>>(stream);
     }
 }
