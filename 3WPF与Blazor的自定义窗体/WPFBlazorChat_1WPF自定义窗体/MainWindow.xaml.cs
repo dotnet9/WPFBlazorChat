958a244a744d57a379f0279fbb34d9f1aa0cd62c
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-11-05 23:42:50 +0800

    添加WPF自定义窗体实现；

---
+++ 3WPF与Blazor的自定义窗体/WPFBlazorChat_1WPF自定义窗体/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace WPFBlazorChat;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+
+    private void MoveWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 1)
+        {
+            this.DragMove();
+        }
+        else
+        {
+            MaximizeWindow_Click(null, null);
+        }
+    }
+
+    private void CloseWindow_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeWindow_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    }
+}
