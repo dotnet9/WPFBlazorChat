4dabd91bb6826dc71616a7ce46ba6b86776c82d1
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-11-06 23:30:11 +0800

    添加消息通知初始代码，方便后面修改对比；

---
+++ 5WPFBlazor消息通知/WPFBlazorChat/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace WPFBlazorChat;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        serviceCollection.AddMasaBlazor();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
